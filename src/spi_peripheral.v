ddf21ed179fbe15e0195208cd46e0e3b86d25e62
Sebastian Nava Urribarri <snavaurr@uwaterloo.ca>
2025-06-25 10:59:10 -0400

    lets see if this will be ok

--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -4,16 +4,6 @@
  */
 
 `default_nettype none
-module counter (  input counter_clock, input rstn, output reg[15:0] out);    
-
-  always @ (posedge counter_clock) begin
-    if (! rstn)
-      out <= 0;
-    else
-      out <= out + 1;
-  end
-
-endmodule
 
 module spi_peripheral (
     input  wire cs,      // chip select
@@ -21,7 +11,7 @@ module spi_peripheral (
     input wire copi, // master out slave in
     input  wire rst,    // reset_n - low to reset
     input wire clk, // system clock
-    output reg [15:0] out, // final output
+  //  output reg [15:0] out, // final output
 
     output reg [7:0] en_reg_out_7_0,
     output reg [7:0] en_reg_out_15_8,
@@ -32,88 +22,103 @@ module spi_peripheral (
     //already in here so must be important
 );
 
-   parameter transcation_ready = 0'b0;
-   parameter transaction_complete = 0'b0;
+   reg transaction_ready = 1'b0;
    reg [2:0] sclk_synchronised; 
    reg [2:0] cs_syncrhonised;
    reg[2:0] copi_synchronised;
     // because we need both the oldest and second oldest values
 
-    reg [0:0] read_or_write;     // one for read / write
-    reg [6:0] adress_bits;     // one for adress
+    reg read_or_write;     // one for read / write
+    reg [5:0] address_bits;     // one for address
     reg [7:0] information_bits;     // one for information
     reg [15:0] all_bits;     // one register for everything
+    reg [15:0] counter;
 
+    wire cs_falls = (cs_syncrhonised[1] == 1'b1 ) & (cs_syncrhonised[2] == 0'b1);
+    wire cs_rises = (cs_syncrhonised[1] == 0'b1 ) & (cs_syncrhonised[2] == 1'b1);
+
+    wire sclk_rising  = (sclk_synchronised[2:1] == 2'b01);
+    wire sclk_falling = (sclk_synchronised[2:1] == 2'b10);
 // block for signal scyhrnoisation
-    always@(posedge sclk or negedge sclk)
+
+always @(posedge clk or negedge rst) 
+begin
+    if (!rst) begin
+        sclk_synchronised <= 3'b000;
+        cs_syncrhonised   <= 3'b000;
+        copi_synchronised <= 3'b111;
+    end else begin
+        sclk_synchronised <= {sclk_synchronised[1:0], sclk};
+        cs_syncrhonised   <= {cs_syncrhonised[1:0], cs};
+        copi_synchronised <= {copi_synchronised[1:0], copi};
+    end
+end
+
+always@(posedge sclk or negedge rst)
+begin
     if (!rst)
     begin
+        read_or_write <= 1'b0;
+        address_bits <= 6'b000000;
+        information_bits <= 8'h00;
+        all_bits <= 16'h0000;
+        counter <= 0;
+        transaction_ready <= 1'b0;
         en_reg_out_7_0 <= 8'h00;
         en_reg_out_15_8 <= 8'h00;
-        en_reg_pwm_7_0 <= 8'h00;
+        en_reg_pwm_7_0  <= 8'h00;
         en_reg_pwm_15_8 <= 8'h00;
         pwm_duty_cycle <= 8'h00;
-
-        sclk_synchronised <= 2'b00;
-        cs_syncrhonised <= 2'b00;
-        copi_synchronised <= 2'b11;
-
-        transaction_ready <= 1'b0; 
-        transaction_complete <= 1'b1;
     end
 
     // signal sychnornisation portion
-    begin
-        sclk_synchronised <= {sclk_synchronised[1:0], sclk}; // second oldest and oldest values
-        cs_syncrhonised <= {cs_syncrhonised[1:0], cs};
-        copi_synchronised <= {copi_synchronised[1:0], copi};
-     end
-
-// block for transport 
-
-    always@(posedge sclk or negedge sclk)
-    begin
-// if not reset, activate signal clock and select a preipheral by setting chip selec to 0
-// after transcation is finished, chip select returns to 1
-// signal slock sclk determines when data bits are sent and read
-// peripheral can confugyre if its rising edge or falling edge
-//  sample copi, sclk and nCS at every rising edge of the internal clk
-// transition identification positive edge is (2nd oldest is high) and (1st oldest is low)
-// negative edge = (2nd oldest = low) and (1st oldest = high)
-    if (!rst)
-    begin
-        en_reg_out_7_0 <= 8'h00;
-        en_reg_out_15_8 <= 8'h00;
-        en_reg_pwm_7_0 <= 8'h00;
-        en_reg_pwm_15_8 <= 8'h00;
-        pwm_duty_cycle <= 8'h00;
-
-        sclk_synchronised <= 2'b00;
-        cs_syncrhonised <= 2'b00;
-        copi_synchronised <= 2'b11;
-
-        transaction_ready <= 1'b0; 
-        transaction_complete <= 1'b1;
-    end
-    else if((sclk_synchronised[1] & 1'b0 ) & (sclk_synchronised[0] ^ 0'b0))
+    else 
     begin
 
-    end
-    else if ((sclk_synchronised[1] ^ 0'b0 ) & (sclk_synchronised[0] & 1'b0))
-    begin
-        
-    end
-
-    end
-
-// transaction stuff
-    if (transcation_ready) 
-    begin
-        if(all_bits[15])
+    if (cs_falls) // get rid of everything in the register to get ready for next time
         begin
-        
+        read_or_write <= 1'b0;   
+        address_bits <= 6'b0;   
+        information_bits <= 8'b0;   
+        all_bits <= 16'b0;    
+        counter <=0;
         end
-
+    else if((cs_syncrhonised[2] == 1'b0) & (!transaction_ready))
+    begin
+        if(sclk_rising)
+        begin
+            if (counter == 0)
+            begin
+            read_or_write <= copi_synchronised[2]; // take in the first bit
+            end 
+            else if (counter < 8)
+            begin
+                address_bits <= {address_bits[4:0], copi_synchronised[2]}; // take address, shuffling everything over
+            end
+            else if (counter < 16)
+            begin
+                information_bits <= {information_bits[6:0], copi_synchronised[2]}; // take info, shuffling everything over
+            end
+        counter <= counter + 1; // increment coutner until we get all bits we need
+        end
+        if (counter == 15)
+        begin
+            transaction_ready <= 1'b1;
+        end 
+// transaction stuff
+    if (transaction_ready && (counter == 16)) 
+    begin
+        all_bits <= {read_or_write, address_bits, information_bits};
+        case(address_bits)
+        6'b000000: en_reg_out_7_0<=information_bits;
+        6'b000001: en_reg_out_15_8<=information_bits;
+        6'b000010: en_reg_pwm_7_0<=information_bits;
+        6'b000011: en_reg_pwm_15_8<=information_bits;
+        6'b000100: pwm_duty_cycle<=information_bits;
+        endcase
+    transaction_ready <= 1'b0;
     end
-
+    end
+end 
+end
 endmodule
